9e55ff5704f59ff4692c128e94b76bdd881c7cf1
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2025-03-06 19:25:59 -0600

    Add a `user_agent` setting (#46)

--- tap_pypistats/tap.py
+++ tap_pypistats/tap.py
@@ -5,6 +5,7 @@ from __future__ import annotations
 import argparse
 import dataclasses
 import datetime
+import importlib.metadata
 import json
 import logging
 import pathlib
@@ -16,6 +17,9 @@ import requests.adapters
 import requests_cache
 
 BASE_URL = "https://pypistats.org/api"
+VERSION = importlib.metadata.version("tap-pypistats")
+
+DEFAULT_USER_AGENT = f"tap-pypistats/{VERSION}"
 
 handler = logging.StreamHandler(sys.stderr)
 handler.setFormatter(logging.Formatter("%(levelname)s: %(message)s"))
@@ -29,6 +33,7 @@ class Config:
     """Configuration for the package."""
 
     packages: list[str]
+    user_agent: str
 
 
 class TapNamespace(argparse.Namespace):
@@ -36,13 +41,14 @@ class TapNamespace(argparse.Namespace):
 
     config: pathlib.Path
 
-    def to_config(self: TapNamespace) -> Config:
+    def get_config(self: TapNamespace) -> Config:
         """Convert the namespace to a configuration."""
         with self.config.open(encoding="utf-8") as file:
             data = json.load(file)
 
             return Config(
                 packages=data["packages"],
+                user_agent=data.get("user_agent", DEFAULT_USER_AGENT),
             )
 
 
@@ -74,10 +80,12 @@ def iter_python_minor(
     session: requests.Session,
     base_url: str,
     package_name: str,
+    *,
+    user_agent: str = DEFAULT_USER_AGENT,
 ) -> t.Generator[dict[str, t.Any], None, None]:
     """Iterate over the data for a package."""
     url = f"{base_url}/packages/{package_name}/python_minor"
-    response = session.get(url, timeout=60)
+    response = session.get(url, timeout=60, headers={"User-Agent": user_agent})
     response.raise_for_status()
     data = response.json()
     yield from data["data"]
@@ -99,10 +107,14 @@ def iter_system(
 def iter_packages(
     base_url: str,
     packages: t.Iterable[str],
+    *,
+    user_agent: str = DEFAULT_USER_AGENT,
 ) -> t.Generator[dict[str, t.Any], None, None]:
     """Iterate over the data for a list of packages.
 
+    :param str base_url: The base URL for the API.
     :param Iterable[str] packages: The names of the packages.
+    :param str user_agent: The user agent to make requests
     :rtype: Iterator[dict]
     """
     now = datetime.datetime.now(datetime.timezone.utc).isoformat()
@@ -116,7 +128,7 @@ def iter_packages(
         "key_properties": ["category", "date", "package"],
     }
     for package in packages:
-        for record in iter_python_minor(session, base_url, package):
+        for record in iter_python_minor(session, base_url, package, user_agent=user_agent):
             yield {
                 "type": "RECORD",
                 "stream": "python_minor",
@@ -152,15 +164,19 @@ def sync_packages(
     *,
     cache_name: str = "pypistats",
     stream: t.IO[str] = sys.stdout,
+    user_agent: str = DEFAULT_USER_AGENT,
 ) -> t.Generator[dict[str, t.Any], None, None]:
     """Sync data for a list of packages.
 
+    :param str base_url: The base URL for the API.
     :param Iterable[str] packages: The names of the packages.
     :param str cache_name: The name of the cache.
+    :param IO[str] stream: The stream to write to.
+    :param str user_agent: The user agent to make requests with.
     :rtype: Iterator[dict]
     """
     requests_cache.install_cache(cache_name)
-    for message in iter_packages(base_url, packages):
+    for message in iter_packages(base_url, packages, user_agent=user_agent):
         write_message(message, stream=stream)
 
 
@@ -177,7 +193,7 @@ def main() -> None:
     args = parser.parse_args(namespace=TapNamespace())
 
     try:
-        config = args.to_config()
+        config = args.get_config()
     except json.JSONDecodeError as error:
         logger.error("Configuration file is not valid JSON: %s", error)
         sys.exit(1)
@@ -192,6 +208,7 @@ def main() -> None:
         BASE_URL,
         config.packages,
         stream=sys.stdout,
+        user_agent=config.user_agent,
     )
 
 
--- tests/test_config.py
+++ tests/test_config.py
@@ -1,11 +1,11 @@
-from tap_pypistats.tap import Config, TapNamespace
+from tap_pypistats.tap import DEFAULT_USER_AGENT, Config, TapNamespace
 
 
-def test_tap_namespace_to_config(tmp_path):
+def test_tap_namespace_get_config(tmp_path):
     """It should convert a namespace to a config."""
     config = tmp_path / "config.json"
     config.write_text('{"packages": ["foo", "bar"]}')
 
     namespace = TapNamespace(config=config)
 
-    assert namespace.to_config() == Config(packages=["foo", "bar"])
+    assert namespace.get_config() == Config(packages=["foo", "bar"], user_agent=DEFAULT_USER_AGENT)
